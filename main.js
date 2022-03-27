529ca8b6de2b1aee0b5e11f7e35a80b3f4accbbb
Ishaan <94088187+Ishaaa113@users.noreply.github.com>
2022-03-27 19:16:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+player1_name = localStorage.getItem("Player1_name");
+player2_name = localStorage.getItem("Player2_name");
+
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name +" : ";
+document.getElementById("player2_name").innerHTML = player2_name +" : ";
+
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
+
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn" + player2_name ;
+
+
+
+function send() {
+    ge
+}
